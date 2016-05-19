abfd9a6c03d87ad8ba72abeacb47238f2aafcca9
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-05-19 14:49:07 +0300

    minor rename

--- src/test/java/net/cserny/bdd/stringcalculator/NumberAdderSpec.java
+++ src/test/java/net/cserny/bdd/stringcalculator/NumberAdderSpec.java
@@ -1,17 +1,15 @@
 package net.cserny.bdd.stringcalculator;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by user on 19.05.2016.
  */
 public class NumberAdderSpec
 {
-    private int getAdditionResult(int[] numbers) throws NegativesNotAllowedException
+    private int getSum(int[] numbers) throws NegativesNotAllowedException
     {
         NumberAdder adder = new NumberAdderImpl();
         return adder.add(numbers);
@@ -20,14 +18,14 @@ public class NumberAdderSpec
     @Test
     public void givenNumberArrayWhenAddingThenReturnTheirSum() throws Exception
     {
-        int result = getAdditionResult(new int[] {1, 2, 3});
+        int result = getSum(new int[]{1, 2, 3});
         assertEquals(6, result);
     }
 
     @Test(expected = NegativesNotAllowedException.class)
     public void givenNegativeNumbersStringWhenAddingThenThrowNegativesNotAllowedException() throws Exception
     {
-        getAdditionResult(new int[] {-1, 3, -5});
+        getSum(new int[]{-1, 3, -5});
     }
 
     @Test
@@ -44,7 +42,7 @@ public class NumberAdderSpec
     @Test
     public void givenNumberInStringBiggerThan1000WhenAddingThenIgnoreIt() throws Exception
     {
-        int result = getAdditionResult(new int[] {1, 2, 1003});
+        int result = getSum(new int[]{1, 2, 1003});
         assertEquals(3, result);
     }
 }
