9916c51960a7970787261bfb8bfa629148fdda9b
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-05-20 09:33:42 +0300

    started from scratch for tdd

--- src/main/java/net/cserny/bdd/stringcalculator/NumberAdder.java
+++
@@ -1,9 +0,0 @@
-package net.cserny.bdd.stringcalculator;
-
-/**
- * Created by user on 18.05.2016.
- */
-public interface NumberAdder
-{
-    public int add(int[] numbers) throws NegativesNotAllowedException;
-}
--- src/main/java/net/cserny/bdd/stringcalculator/NumberParser.java
+++
@@ -1,9 +0,0 @@
-package net.cserny.bdd.stringcalculator;
-
-/**
- * Created by user on 18.05.2016.
- */
-public interface NumberParser
-{
-    public int[] getNumbers(String expression);
-}
