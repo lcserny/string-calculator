26e1e7ff55963913fb1361255ab21647017fa06f
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-05-20 09:30:39 +0300

    left only cucumber runner

---
+++ src/test/java/net/cserny/bdd/stringcalculator/StringCalculatorRunner.java
@@ -0,0 +1,16 @@
+package net.cserny.bdd.stringcalculator;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by user on 19.05.2016.
+ */
+@RunWith(Cucumber.class)
+public class StringCalculatorRunner { }
